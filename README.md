33dbcd917884720f4da86963de2e37710c8e2060
lesatdgl <les@dunawaygroupllc.com>
2021-07-15 20:27:56 -0400

    Begin class defn

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tda-journal
 Uses ThinkOrSwim API to grab trades and results to create options trading journal
+Flask is used for dashboard
+SqlLite is used for storage
--- tda_journal/app.py
+++ tda_journal/app.py
@@ -2,6 +2,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""echo "Running Flask App"
+flask run
+"""
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
